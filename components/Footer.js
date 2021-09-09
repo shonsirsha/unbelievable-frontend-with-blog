699359533850835630656cd6bb958cccf2b5365c
shonsirsha <seangeekpro@gmail.com>
2021-09-09 21:00:50 +0700

    new footer created

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,37 @@
 import styles from "./Footer.module.css";
-import Image from "next/image";
+import { Image } from "react-bootstrap";
+import styled from "styled-components";
+import { TextSecondary } from "./Typography/Text";
+const StyledFooter = styled.footer`
+	margin-top: auto;
+	width: 100%;
+	height: 100px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 16px;
+	border-top: 3px solid #2b72f0;
+`;
+
 export default function Footer() {
 	return (
-		<>
-			<footer className={styles.footer}>
-				<Image
-					width={"20px"}
-					height={"20px"}
-					src="/netliheart.svg"
-					alt="Netlify Logo"
-					className={styles.logo}
-				/>
-				Three-Sigma Technologies | Unbelievable.id Developer Preview
-			</footer>
-		</>
+		<StyledFooter className="bg-primary1">
+			{/* <Image
+				width={"20px"}
+				height={"20px"}
+				src="/netliheart.svg"
+				alt="Netlify Logo"
+				className={styles.logo}
+			/> */}
+			<Image
+				width={"220px"}
+				height={"24px"}
+				src="/images/logo.png"
+				alt="logo"
+			/>
+			<TextSecondary className="text-white ">
+				© Unbelievable.id, 2021
+			</TextSecondary>
+		</StyledFooter>
 	);
 }
