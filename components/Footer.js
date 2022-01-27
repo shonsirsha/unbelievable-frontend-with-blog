e04a32798bca7a928308cfccf976c2288e832393
shonsirsha <seangeekpro@gmail.com>
2022-01-27 17:17:34 +0100

    rm unused

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,4 @@
 import { useContext, useEffect } from "react";
-import AuthContext from "context/AuthContext";
 import { Image } from "react-bootstrap";
 import styled from "styled-components";
 import { TextPrimary, TextSecondary, TextTertiary } from "./Typography/Text";
