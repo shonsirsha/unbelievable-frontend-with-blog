fe10eea73d561e5237bd31918da7057e8b251738
shonsirsha <seangeekpro@gmail.com>
2022-03-02 04:28:07 +0100

    safely .json() for /api/users

--- pages/api/user.js
+++ pages/api/user.js
@@ -3,20 +3,30 @@ import { API_URL } from "config/index";
 
 export default async function user(req, res) {
 	if (req.method === "POST") {
+		console.log(`Hitting: ${API_URL}/users/me`);
 		if (!req.headers.cookie) {
 			res.status(403).json({ message: "Not Authorized" });
 			return;
 		}
 
 		const { token } = cookie.parse(req.headers.cookie);
-		const strapiRes = await fetch(`${API_URL}/users/me`, {
+		console.log(token);
+
+		let strapiRes = await fetch(`${API_URL}/users/me`, {
 			method: "GET",
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
 		});
-		let user = await strapiRes.json();
-		user.token = token;
+		let user;
+		try {
+			user = await strapiRes.json();
+			user.token = token;
+		} catch (e) {
+			console.log("ERROR", e);
+		}
+		console.log("strapiRes", user);
+		console.log("USER", user);
 		if (strapiRes.ok) {
 			res.status(200).json({ user });
 		} else {
