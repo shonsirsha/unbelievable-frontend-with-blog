0e0675e2f16fcf240ecc09ad03d91660802257e8
shonsirsha <seangeekpro@gmail.com>
2022-09-05 23:51:05 +0200

    individual blog post page WIP

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,13 +1,77 @@
 import React from "react";
 import { API_URL } from "config";
 import { parseCookies } from "utils/cookies";
+import Layout from "components/Layout";
+import Container from "react-bootstrap/Container";
+import styled from "styled-components";
+import {
+	HeadingLG,
+	HeadingMD,
+	HeadingXXS,
+} from "components/Typography/Headings";
+import {
+	TextPrimary,
+	TextSecondary,
+	TextTertiary,
+} from "components/Typography/Text";
+import moment from "moment";
+
+const StyledContainer = styled(Container)`
+	width: 100%;
+	justify-content: center;
+	padding-top: 140px;
+	padding-bottom: 56px;
+	overflow: hidden;
+`;
+
+const TopicPill = styled.div`
+	padding: 11px 40px;
+	min-width: 96px;
+	background: #dbdbdb;
+	border-radius: 17px;
+`;
+
+const TopicText = styled(HeadingXXS)`
+	font-size: 13px;
+`;
 
 export default function BlogPost({ slug, blogPost }) {
 	return (
-		<div
-			className="ck-content p-4"
-			dangerouslySetInnerHTML={{ __html: blogPost.content }}
-		></div>
+		<Layout
+			background="#171b2d"
+			title={blogPost.title}
+			description="Semua kelas yang ada di Unbelievable. Ikuti sekarang! | Blog Unbelievable.id"
+		>
+			<StyledContainer>
+				<HeadingMD as="h1">{blogPost.title}</HeadingMD>
+
+				<div className="d-flex flex-col my-3">
+					<TextPrimary className="text-gray2">
+						{blogPost.readTime === 0 ? `1` : blogPost.readTime} Menit
+					</TextPrimary>
+
+					<TextPrimary className="text-gray2 ml-4">
+						{moment(blogPost.createdAt).format("D MMMM YYYY")}
+					</TextPrimary>
+				</div>
+
+				<div className="d-flex">
+					{blogPost.blogTopics.map((topic, ix) => (
+						<TopicPill
+							className={`${ix > 0 ? `ml-2` : `ml-0`}`}
+							key={topic.topicId}
+						>
+							<TopicText>{topic.topicName}</TopicText>
+						</TopicPill>
+					))}
+				</div>
+
+				<div
+					className="ck-content mt-4"
+					dangerouslySetInnerHTML={{ __html: blogPost.content }}
+				></div>
+			</StyledContainer>
+		</Layout>
 	);
 }
 
